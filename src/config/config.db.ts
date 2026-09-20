e6488de4a8bb36acb84488b204539092e5c2830f
adityamottha <adityamottha5@gmail.com>
2026-09-20 13:32:22 +0530

    Config MONGO_DB database

---
+++ src/config/config.db.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import "./config.env.js";
+
+export const connectDB = async (): Promise<void> => {
+  try {
+    const mongoUri = process.env.MONGODB_URI;
+
+    if (mongoUri === undefined) {
+      throw new Error("MONGODB_URI is not defined");
+    }
+
+    const connectionInstance = await mongoose.connect(mongoUri);
+
+    console.log(
+      "DATABASE CONNECTED SUCCESSFULLY!",
+      connectionInstance.connection.host,
+    );
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("DATABASE CONNECTION FAILED!", error.message);
+    } else {
+      console.error("DATABASE CONNECTION FAILED!", error);
+    }
+
+    process.exit(1);
+  }
+};
